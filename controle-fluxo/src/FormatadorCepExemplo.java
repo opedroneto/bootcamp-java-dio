f9df428c9a25353c96b5e07669703f12420084e2
Pedro Vaz Neto <141879425+opedroneto@users.noreply.github.com>
2024-05-13 22:46:16 -0300

    Add files via upload

---
+++ controle-fluxo/src/FormatadorCepExemplo.java
@@ -0,0 +1,22 @@
+public class FormatadorCepExemplo {
+  public static void main(String[] args) {
+    try {
+      String cepFormatado = formatarCep("2376506");
+      System.out.println(cepFormatado);
+    } catch (CepInvalidoException e) {
+      System.out.println("O CEP não corresponde as regras de negócio");
+      // e.printStackTrace();
+
+    }
+    
+  }
+
+  static String formatarCep(String cep) throws CepInvalidoException {
+    if (cep.length() != 8)
+      throw new CepInvalidoException();
+      
+      // simulando um cep formatado
+      return "23.765-064";
+  }
+  
+}
