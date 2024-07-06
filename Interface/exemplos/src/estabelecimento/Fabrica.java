ddd5b66828c42676c4bb67b5c4468a4aeed5fec5
Pedro Vaz Neto <141879425+opedroneto@users.noreply.github.com>
2024-07-05 21:35:16 -0300

    Add files via upload

---
+++ Interface/exemplos/src/estabelecimento/Fabrica.java
@@ -0,0 +1,25 @@
+package estabelecimento;
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Deskjet;
+import equipamentos.impressora.Laserjet;
+import equipamentos.impressora.Impressora;
+import equipamentos.multifuncional.EquipamentoMultifuncional;
+
+public class Fabrica {
+	public static void main(String[] args) {
+		EquipamentoMultifuncional em = new EquipamentoMultifuncional();
+		
+		Deskjet deskjet = new Deskjet();
+		
+		Impressora impressora = em;
+		Digitalizadora digitalizadora = em;
+		Copiadora copiadora = em;
+		
+		impressora.imprimir();
+		digitalizadora.digitalizar();
+		copiadora.copiar();
+	}
+
+}
